d0ee56d9b9cc01a04c3e1b695182612f36667435
ramya.reddy161 <ramya.reddy161@gmail.com>
2018-07-30 03:49:37 +0530

    commit to deploy

--- colloquium/settings.py
+++ colloquium/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for colloquium project.
 
@@ -164,3 +165,15 @@ WEBPACK_LOADER = {
     }
 }
 
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
